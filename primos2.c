d49a0b15f151f057aa483d631ae4e2ac59ad89cd
dvherrera <dvherrera@yahoo.com>
2022-05-22 21:23:37 +0200

    Primos versión con do while

---
+++ primos2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int esPrimo(int);
+
+int main(void) {
+
+   int numero;
+   int i=2;
+   printf("Introduzca un numero por teclado:\n");
+   scanf("%d",&numero);
+
+   if(numero<0){
+      printf("\nEste programa solo es valido para numeros positivos");
+      return 0;
+   }
+
+   if(esPrimo(numero)) {
+      printf("\nEl numero %d es primo",numero);
+      return 0;
+   } else {
+      printf("\nEl numero %d no es primo",numero);
+      return 0;
+   }
+}
+
+int esPrimo(int numero) {
+   if (numero==0 || numero==1)
+      return 0;
+   printf("\nComprobamos hasta el numero %d",numero/2);
+   int i=2;
+   do {
+      if(numero%i==0)
+         return 0;
+      i++;
+   } while(i < numero/2);
+
+   /*for(int i = 2; i < numero/2; i++) {
+      if (numero % i == 0)
+         return 0;
+   }*/
+   return 1;
+
+}
